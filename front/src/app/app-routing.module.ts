73ae10a9445662474cffe6678c7d791e820e6a4c
romingo <romain.grenier@ynov.com>
2023-04-14 10:10:39 +0200

    merge

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'register', component: RegisterComponent },
+  //
  
 
 ]
