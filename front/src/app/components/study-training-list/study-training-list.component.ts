b1fc23c63ef3179aa02f58859e5e3d65f1d44175
Clément Bilger <clement.bilger25@gmail.com>
2023-04-14 19:43:01 +0200

    bonne route + adapation

--- front/src/app/components/study-training-list/study-training-list.component.ts
+++ front/src/app/components/study-training-list/study-training-list.component.ts
@@ -8,13 +8,109 @@ interface LabelsAndRomesData {
   label: string;
   romes: string[];
 }
+export interface Job {
+  ideaType: string;
+  title: string;
+  longTitle: string | null;
+  id: number | null;
+  idRco: number | null;
+  idRcoFormation: number | null;
+  contact: any; // Type spécifique à définir pour les détails de contact
+  place: {
+    distance: number;
+    insee: string;
+    zipCode: string;
+    city: string;
+    latitude: number;
+    longitude: number;
+    fullAddress: string;
+  };
+  company: {
+    name: string;
+    siret: string | null;
+  };
+  diplomaLevel: string | null;
+  diploma: string | null;
+  cfd: string | null;
+  rncpCode: string | null;
+  rncpLabel: string | null;
+  rncpEligibleApprentissage: string | null;
+  period: string | null;
+  capacity: string | null;
+  createdAt: string | null;
+  lastUpdateAt: string | null;
+  onisepUrl: string | null;
+  url: string;
+  job: {
+    id: string;
+    creationDate: string;
+    description: string;
+    contractType: string;
+    contractDescription: string;
+    duration: string;
+  };
+  romes: { code: string }[];
+  nafs: { code: string; label: string }[];
+  training: any | null; // Type spécifique à définir pour les détails de formation
+}
 
 export interface Formation {
+   ideaType: string;
   title: string;
-  ideaType: string;
+  longTitle: string;
+  id: string;
+  idRco: string;
+  idRcoFormation: string;
+  contact: null | any; // Remplacer 'any' par le type approprié si possible
+  place: {
+    distance: null | any; // Remplacer 'any' par le type approprié si possible
+    fullAddress: string;
+    latitude: string;
+    longitude: string;
+    city: string;
+    address: string;
+    cedex: null | any; // Remplacer 'any' par le type approprié si possible
+    zipCode: string;
+    departementNumber: string;
+    region: string;
+    insee: string;
+  };
+  company: {
+    name: string;
+    siret: string;
+    id: string;
+    uai: string;
+    headquarter: {
+      id: string;
+      uai: string;
+      siret: string;
+      place: {
+        address: string;
+        cedex: null | any; // Remplacer 'any' par le type approprié si possible
+        zipCode: string;
+        city: string;
+      };
+      name: string;
+    };
+    place: null | any; // Remplacer 'any' par le type approprié si possible
+  };
+  diplomaLevel: string;
+  diploma: string;
+  cfd: string;
+  rncpCode: string;
   rncpLabel: string;
+  rncpEligibleApprentissage: boolean;
+  period: null | any; // Remplacer 'any' par le type approprié si possible
+  capacity: null | any; // Remplacer 'any' par le type approprié si possible
+  createdAt: string;
+  lastUpdateAt: string;
   onisepUrl: string;
-  zipCode: string
+  url: null | any; // Remplacer 'any' par le type approprié si possible
+  job: null | any; // Remplacer 'any' par le type approprié si possible
+  romes: Array<{ code: string }>;
+  nafs: null | any; // Remplacer 'any' par le type approprié si possible
+  training: null | any; // Remplacer 'any' par le type approprié si possible
+  cleMinistereEducatif: string;
 }
 
 @Component({
@@ -27,13 +123,15 @@ export class StudyTrainingListComponent implements OnInit {
   public apiData: any;
   public metiers: any;
   public formations: Formation[] = [];
+  public jobs: Job[] = [];
+
   public selectedMetier: string = ''; 
-  public departement: string = ''; // Ajouter cette déclaration
+  public insee: string = ''; // Ajouter cette déclaration
 
 
   public onSubmit(): string {
     
-    return this.departement
+    return this.insee
   }
   public getSelectedMetier(): void {
     this.api.selectedMetier = this.selectedMetier;
@@ -44,8 +142,8 @@ export class StudyTrainingListComponent implements OnInit {
       console.log(this.apiData.toString(), 'apiData')
       
     })
-    console.log(this.departement + 'dep');
-    this.api.getFormationsParRegion(this.departement, this.apiData).subscribe(
+    console.log(this.insee + 'dep');
+    /*this.api.getFormationsParRegion(this.departement, this.apiData).subscribe(
       data => {
         // Traitement des données de réponse de l'API
         console.log(data.results[0].place.zipCode + 'forma /region');
@@ -56,7 +154,12 @@ export class StudyTrainingListComponent implements OnInit {
         // Gestion des erreurs
         console.error(error);
       }
-    );
+    );*/
+    this.api.getJobsParInsee(this.insee, this.apiData).subscribe(data => {
+      
+      this.jobs = data.peJobs.results;
+      console.log(this.jobs[0].title + '-- job ');
+    });
   
   }
 
@@ -75,6 +178,8 @@ export class StudyTrainingListComponent implements OnInit {
     this.api.getAllMetiers().subscribe(data => {
       this.metiers = data.metiers;
     });
+
+
 
   }
 
