fff441f979b1244b10394850b796367ea5acc38f
romingo <romain.grenier@ynov.com>
2023-04-14 13:27:33 +0200

    connexion + switch nom prenom

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { StudyTrainingListComponent } from './components/study-training-list/study-training-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { firebaseConfig } from '../environments/firebase-config';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat';
@@ -14,18 +15,6 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './components/register/register.component'
-
-
-
-// Ajoutez votre configuration Firebase ici
-const firebaseConfig = {
-  apiKey: "AIzaSyDrY7-nDW2px7KEZBIwlb3r-LvMRYkwnuM",
-  authDomain: "yfinder-83eb9.firebaseapp.com",
-  projectId: "yfinder-83eb9",
-  storageBucket: "yfinder-83eb9.appspot.com",
-  messagingSenderId: "743126886633",
-  appId: "1:743126886633:web:3cd45a4e319ca9864a6878"
-}
 
 
 @NgModule({
@@ -41,8 +30,8 @@ const firebaseConfig = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule, // Ajoutez ReactiveFormsModule ici
-    AngularFireModule.initializeApp(firebaseConfig), // Ajoutez AngularFireModule ici avec la configuration Firebase
+    ReactiveFormsModule, 
+    AngularFireModule.initializeApp(firebaseConfig), 
     AngularFireAuthModule,
     AngularFirestoreModule,
     
--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+  async onSubmit() {
+    const { email, password } = this.loginForm.value;
+    try {
+      const { user } = await this.afAuth.signInWithEmailAndPassword(email, password);
+      console.log('User logged in successfully:', user);
+  
+      // Récupération des informations de l'utilisateur depuis Cloud Firestore
+      const userDoc = await this.afs.collection('users').doc(user?.uid).get().toPromise();
+      if (userDoc?.exists) {
+        const userData: { nom: string, prenom: string } = userDoc.data() as { nom: string, prenom: string };
+        const nom = userData.nom;
+        const prenom = userData.prenom;
+        // console.log('Nom:', nom);
+        // console.log('Prénom:', prenom);
+  
+        // Redirection vers la page d'accueil
+        this.router.navigate(['']);
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  }
+  
 }
